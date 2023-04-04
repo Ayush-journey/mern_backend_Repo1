ea545570c7a1b1c9231cc7db9ac7040c2ee525e9
Ayush-journey <110773530+Ayush-journey@users.noreply.github.com>
2023-04-04 22:12:38 +0530

    various correction implemented

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const mongodb = require('mongoose'); 
 const router = require('./routes/router');
 const app = express();
+const cors = require('cors');
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 const Port = process.env.PORT || 5000;
 
